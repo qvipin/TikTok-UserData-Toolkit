56e046e42477f7fbd1335f24936ef2d7820e9ce8
Vipin B <vipin@vipin.xyz>
2025-01-19 11:34:17 -0500

    Initial Commit; Tiktok Video Download script & Readme

---
+++ README.md
@@ -0,0 +1,56 @@
+# TikTok User Data Toolkit
+
+A collection of scripts to parse TikTok user data. As of January 19, 2025, TikTok has been banned, leaving users with only the option to download their data from the app or website. This user data contains valuable information, including messages, comments, posts, and more, which you may want to save and analyze.
+
+This toolkit helps you efficiently parse and extract meaningful insights from your downloaded TikTok data.
+
+## Installation
+
+To get started, clone the repository and navigate to the scripts directory:
+
+```bash
+git clone https://github.com/qvipin/TikTok-UserData-Toolkit.git
+cd TikTok-UserData-Toolkit/scripts
+```
+
+### Usage
+
+Run any script using the following command:
+
+```bash
+./<script-name> <path-to-json>
+```
+
+For example:
+
+```bash
+./tiktok-video_download.py ~/Downloads/user_data_tiktok.json
+```
+
+## Available Scripts
+
+The toolkit includes various scripts to parse different types of TikTok data. Some examples include:
+
+- tiktok-video_download.py – Extracts and downloads all your TikTok videos.
+
+## Contributing
+
+Contributions are welcome! Feel free to submit pull requests or report issues to help improve the toolkit.
+
+## License
+
+Copyright (C) 2025 qvipin
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
---
+++ scripts/tiktok-video_download.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2025 qvipin
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+import json
+import urllib.request as download
+import urllib.error as download_error
+import http.client as download_error2
+import os, sys
+
+if len(sys.argv) > 1:
+    json_file = sys.argv[1]
+else:
+    print("Usage: ./tiktok-video_download.py <JSON File>")
+    sys.exit(1)
+
+"""JSON File"""
+with open(json_file, 'r') as file: 
+    user_data_tiktok = file.read()
+
+"""JSON Parsing"""
+data = json.loads(user_data_tiktok)
+
+video_list = data["Video"]["Videos"]["VideoList"]
+
+"""Downloading the files"""
+
+os.makedirs("./Tiktok_Videos", exist_ok=True)
+
+def vid_name_gen(video_url, vid_date): # Gives the correct file extension for the file
+    if "jpeg" in video_url: # ensuring the file isnt a jpeg and messing up the download
+        vid_name = vid_date + ".jpeg" 
+    else: 
+        vid_name = vid_date + ".mp4" 
+    return vid_name
+
+def file_path_from_name(vid_name): # Deriving file path from name
+    folder_name = "./Tiktok_Videos"
+    file_path = os.path.join(folder_name, vid_name)
+    return file_path
+
+
+
+for item in range(1, len(video_list)):
+    try:
+        vid_date = ((video_list[item]['Date']).replace(" ", "_")).replace(":", "-") # name & date
+        video_url = video_list[item]['Link']             
+        
+        vid_name = vid_name_gen(video_url, vid_date) 
+        
+        file_path = file_path_from_name(vid_name)
+        
+        download.urlretrieve(video_url, file_path)
+        print(f"[*] Downloading: {vid_name}, currently on item {item}/{len(video_list)}")
+    except (download_error.HTTPError, download_error2.InvalidURL, download_error.URLError, ValueError):
+        # the code below checks for newlines in url as that is how slideshow image links are represented, so we need to download them in a different way
+        if "\n" in video_url: 
+            urls = video_url.strip().split("\n")
+            for url in urls:
+                try:
+                    vid_name = vid_name_gen(url, vid_date) 
+        
+                    file_path = file_path_from_name(vid_name)
+                    
+                    download.urlretrieve(url, file_path)
+                    print(f"[*] Downloading: {vid_name}, currently on item {item}/{len(video_list)}")
+                except (download_error.HTTPError, download_error2.InvalidURL, download_error.URLError, ValueError):
+                    print(f"[*] Oops, we can't download a part of {item}/{len(video_list)}! (FYI: This item is a slideshow.)")
+
+        else:
+            print(f"[*] Oops, we can't download item {item}/{len(video_list)}!")
+            continue
+    except KeyboardInterrupt:
+        print("[*] CTRL + C Detected, quitting the program...")
+
+print("[*] Done! Go check the Tiktok_Videos folder for your videos!")
